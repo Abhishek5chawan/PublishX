b4d273fb3d613b409d99ac62c3bccbae4a600f16
Abhishek_Chawan <abhishekchawan907@gmail.com>
2024-09-13 11:04:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-        {/* hello world */}
         TODO:  <Outlet />
         </main>
         <Footer />
